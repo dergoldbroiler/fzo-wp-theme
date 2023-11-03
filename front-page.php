004b35260c3e67e451aa475f097d6f07827ba9cd
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-11-03 17:18:28 +0100

    update frontpage

--- front-page.php
+++ front-page.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 
-<div class="container-fluid p-0" id="main">
+<div class="container p-0" id="main">
 
+    <div class="row">
+        <div class="col-12">
+            <?php get_template_part('template-parts/posts/postgrid'); ?>
+        </div>
+    </div>
 
 </div>
 
