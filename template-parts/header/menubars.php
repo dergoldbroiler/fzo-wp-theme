93c0d36475ba556d7b29a8d726c1baaf70e8af24
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-11-08 17:37:11 +0100

    mobile menu animations

--- template-parts/header/menubars.php
+++ template-parts/header/menubars.php
@@ -1,6 +1,6 @@
 <div class="d-flex w-100 justify-content-end align-items-center d-xl-none">
-<div id="menuopener" class="d-flex flex-column justify-content-center align-items-center">
-    <div id="nav-icon" class="d-flex flex-column justify-content-center align-items-center">
+<div id="menuopener" class="d-flex flex-column align-items-center">
+    <div id="nav-icon" class="d-flex flex-column justify-content-center">
         <span></span>
         <span></span>
         <span></span>
