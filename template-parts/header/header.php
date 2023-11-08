6fe2bb10765f4132bc41d801eabb798ae60b5315
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-11-08 16:20:30 +0100

    updated mopbile nav

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -1,16 +1,20 @@
 <header class="container-fluid w-100 p-0" id="header">
   
 
-    <div class="container py-4" id="header-inner-container">
+    <div class="container py-4 px-4 px-xl-0" id="header-inner-container">
         <div class="row">
 
-            <div class="col-6 col-xl-3">
-                <a href="/">
+            <div class="col-9 col-xl-3">
+                <a href="/" class="d-none d-xl-block">
                     <img  src="<?php bloginfo('stylesheet_directory'); ?>/images/FZ_Olpe_Logo.webp" alt="Autohaus Löhr, Fahrzeugzentrum Olpe" class="img-fluid" id="top-logo"/>
                 </a>
+                <a href="/" class="d-block d-xl-none">
+                    <img  src="<?php bloginfo('stylesheet_directory'); ?>/images/FZ_Olpe_Logo_mobile.svg" alt="Autohaus Löhr, Fahrzeugzentrum Olpe" class="img-fluid" id="top-logo"/>
+                </a>
             </div>
-            <div class="col-6 offset-xl-3 col-xl-6">
+            <div class="col-3 offset-xl-3 col-xl-6">
                 <?php get_template_part('template-parts/header/contact','',array()) ?>
+                <?php get_template_part('template-parts/header/menubars','',array()) ?>
             </div>
         </div>
     </div>    
---
+++ template-parts/header/menubars.php
@@ -0,0 +1,9 @@
+<div class="d-flex w-100 justify-content-end align-items-center d-xl-none">
+<div id="menuopener" class="d-flex flex-column justify-content-center align-items-center">
+    <div id="nav-icon" class="d-flex flex-column justify-content-center align-items-center">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div>
+</div>
