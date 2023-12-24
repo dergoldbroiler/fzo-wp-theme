5cbb57a22c0c4ec8229296f2816efc5293d64015
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-12-24 15:02:54 +0100

    update mpbile

--- template-parts/shortcodes/contactboxes.php
+++ template-parts/shortcodes/contactboxes.php
@@ -1,5 +1,5 @@
 
-<div class="posts-grid p-0 w-100 rowmargin m60">
+<div class="posts-grid p-0 w-100 rowmargin m60 mx-5 mx-lg-0">
 
     <?php get_template_part('template-parts/shortcodes/contact'); ?>
     <?php get_template_part('template-parts/shortcodes/fragen'); ?>
