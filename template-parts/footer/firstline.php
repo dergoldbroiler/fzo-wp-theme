399ef783beab267b100f1d37da49c5cf3e08abe5
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-12-27 15:16:21 +0100

    update mobile

--- template-parts/footer/firstline.php
+++ template-parts/footer/firstline.php
@@ -1,18 +1,22 @@
 <div class="container m-auto p-0 m-0">
     <div class="row">
         <div class="col-12">
-        <div class="d-flex w-100 justify-content-start align-items-center py-5">
 
-            <div class="logofooter pe-5">
-                <a href="<?php echo get_home_url(); ?>">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/FZ_Olpe_Logo_mobile.svg" alt="Logo" class="img-fluid">
-                </a>
-            </div>      
-            <div class="menufooter">
-            <?php
-                        wp_nav_menu(array('menu' => 'mainmenu', 'menu_id'=>'mainmenu', 'container' => false, 'menu_class' => 'mleft'));
-                    ?>
+            <div class="d-flex w-100 justify-content-start align-items-center py-5">
+
+                <div class="logofooter pe-5">
+                    <a href="<?php echo get_home_url(); ?>">
+                        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/FZ_Olpe_Logo_mobile.svg" alt="Logo" class="img-fluid">
+                    </a>
+                </div>      
+                <div class="menufooter">
+                <?php
+                        //  wp_nav_menu(array('menu' => 'mainmenu', 'menu_id'=>'mainmenu', 'container' => false, 'menu_class' => 'mleft'));
+                        ?>
+                </div>
+
             </div>
+
         </div>
-        </div>
+    </div>
     </div>
