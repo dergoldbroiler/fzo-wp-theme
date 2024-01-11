4772640cf5f1475c5c706b15dd8d18daad0652a4
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-01-11 16:16:26 +0100

    mobile update

--- template-parts/posts/single.php
+++ template-parts/posts/single.php
@@ -1,6 +1,5 @@
 
-
-<div class="single-post bg-white glide__track" class="" data-glide-el="track">
+<div class="single-post bg-white glide__track" class="" onclick="location.href='<?php echo $args['link']; ?>'">
    
         <?php if(isset($args['bild'])): ?>
 
