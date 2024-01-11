6965938563e387b678dc42788cd9957dd8fd14af
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-01-11 16:30:38 +0100

    mobile update

--- template-parts/shortcodes/notfound.php
+++ template-parts/shortcodes/notfound.php
@@ -1,7 +1,7 @@
 <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center rowmargin p-5" id="yellowbanner">
 <lottie-player src="<?php bloginfo('stylesheet_directory'); ?>/animation_lmltvt78.json" background="transparent"  speed="1" class="notfound-lottie" loop autoplay></lottie-player>
-  <div class="notfound-content">
-    <h2 class="white fs23">Nichts passendes gefunden?</h2>
+  <div class="notfound-content cursor-pointer" onclick="location.href='/kontakt'">
+    <h2 class="white fs23 cursor-pointer">Nichts passendes gefunden?</h2>
     <a href="/kontakt" class="white icon-arrow-white fs16 text-decoration-none">Jetzt Fahrzeug anfragen</a>
   </div>  
 </div>
