356b297ce3cefa023bf8ac9061d2e133b1faef87
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-01-18 07:45:14 +0100

    update

--- footer.php
+++ footer.php
@@ -30,6 +30,20 @@
         </div>  
        
     </div>
+
+     <!-- Kontaktformular -->
+     <div class="container-fluid w-100 rowmargin m80">
+        <div class="container p-0" id="contactform">   
+            <div class="row">
+                <div class="col-12">
+                    <div class="orange-after mb-5 pb-5 text-center">
+                        <h2 class="text-center">So finden Sie uns!</h2>
+                    </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d134728.19684016018!2d7.740659802688244!3d50.95929574466571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sfahrzeugzentrum%20olpe!5e0!3m2!1sde!2sde!4v1702213853208!5m2!1sde!2sde" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>    
+    </div>
 <?php endif; ?>
 
 
