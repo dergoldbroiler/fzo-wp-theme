e1052ddd6f1290a4a493b0ef8c593270fa003571
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-04-27 09:11:53 +0200

    prelive bugfixing

--- template-parts/shortcodes/servicebar.php
+++ template-parts/shortcodes/servicebar.php
@@ -1,21 +1,21 @@
 <div class="d-flex flex-column flex-lg-row justify-content-between my-5 align-items-center mx-4 mx-lg-0" id="servicebar">
 
-    <div class="single-item text-center">
+    <div class="single-item text-center d-flex flex-column align-items-center mb-3 mb-lg-0 pb-5 pb-lg-0">
         <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/servicebar_1.svg" alt="Transparent & Verbindlich" class="servicebar-icon" />
         <p>Transparent & Verbindlich</p>
     </div>
 
-    <div class="single-item text-center">
+    <div class="single-item text-center d-flex flex-column align-items-center mb-3 mb-lg-0 pb-5 pb-lg-0">
         <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/felge.svg" alt="Felgenreparatur" class="servicebar-icon" />
         <p>Felgenreparatur</p>
     </div>
 
-    <div class="single-item text-center">
+    <div class="single-item text-center d-flex flex-column align-items-center mb-3 mb-lg-0 pb-5 pb-lg-0">
         <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/aufbereitung_icon.svg" alt="Fahrzeugaufbereitung" class="servicebar-icon" />
         <p>Fahrzeugaufbereitung</p>
     </div>
 
-    <div class="single-item text-center">
+    <div class="single-item text-center d-flex flex-column align-items-center mb-3 mb-lg-0 pb-5 pb-lg-0">
         <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/Beschichtung_icon.svg" alt="Keramikbeschichtung" class="servicebar-icon" />
         <p>Keramikbeschichtung</p>
     </div>
